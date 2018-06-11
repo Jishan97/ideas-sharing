0d323672f6fd7a87ef57a5a52b145f2fe1893289
unknown <shaikh.jishan1997@gmail.com>
2018-06-11 22:34:44 +0530

    port

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ next();
 // port
     //const port = 5000;
 
-    const port = process.SET.PORT || 5000;
+    const port = process.env.PORT || 5000;
     app.listen(port,()=>{
         console.log(`Server on port ${port}`);
     });
